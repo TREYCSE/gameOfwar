90a93257a2014d3eda000c00bd1ece5383b3da00
TREYCSE <treychelle.weathers2017@gmail.com>
2022-12-22 01:35:01 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-//CREATE DECK OF CARDS
+//DECK OF CARDS
 let suit = ["♠", "♦", "♣", "♥"];
 let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
@@ -6,9 +6,11 @@ class Deck {
     constructor(cards = newDeck()) {
         this.cards = cards;
         }; 
+//this.cards.length can be reffered by this new value to short/hand writing it in the code in the future
     get numberOfCards() {
         return this.cards.length
     }
+//shuffle method is created that will generate a random value from the deck - while giving the cards an index value
     shuffle () {
         for (let i = this.numberOfCards - 1; i > 0; i--) {
             const newIndex = Math.floor(Math.random() * (i + 1))
@@ -17,14 +19,16 @@ class Deck {
             this.cards[i] = oldValue;
         };
     };
+//the last (but randomized) card from the deck of cards is returned
     pop() {
         return this.cards.shift();
     }
+//the cards from the deck are being pushed into the new cards array
     push(card) {
         this.cards.push(card);
     }
 };
-
+//each card is being given a suit
 class Card {
     constructor(suit, value) {
         this.suit = suit;
@@ -60,9 +64,8 @@ let cardValue = {
 let playerDeck, computerDeck, inRound
 
 //GAME ROOM
-//Alert user that they have entered the game and show them the rules that are displayed on the page and the player is prompted to continue
+//alerting user that they have entered the game and show them the rules that are displayed on the page and the player is prompted to continue and layer is prompted to ask to agree to the terms
 alert("You seem to have stumbled into enemy territory! Looks like you're gonna have to play the game of WAR to get out...I am your moderator - Wizard Trey - and I am here to tell you the rules of WAR: The rules of the game are simple... To play, each player reveals the top card in their stack. The player who played the card with the higher rank takes both cards and puts them at the bottom of their stack in an arbitrary order. If there is a tie, then it’s WAR! In the card game each player adds places the top three cards of their stack face down, and then each player reveals the top card again. Whoever wins out of the second reveal takes all of the cards, and if there is another tie the process repeats until there is a winner....May the CARDS be ever in your favor...");
-//Player is prompted to ask to agree to the terms
 let promptAgree = " "; {
     while (promptAgree !== "YES") {
         promptAgree = prompt (
@@ -74,7 +77,7 @@ const input = prompt(".....What's your name, Player??");
 console.log(`Player ${input} has entered the WAR CONSOLE!!`);
 
 class gameRoom {
-//Players are created and a message that the game has started appears in the console
+//players are created and a message that the game has started appears in the console
         constructor(player, computer) {
             this.player1 = player;
             this.player2 = computer;
@@ -124,7 +127,7 @@ function flipCards() {
     console.log(computerCard);
 
     updateDeckCount();
-//Print a message for each “round” of the game showing the cards played by each user, who won the round, and how many cards each player now has.
+//printing message for each “round” of the game showing the cards played by each user, who won the round, and how many cards each player now has.
     if (roundWinner(playerCard, computerCard)) {
         console.log("You frickin Win (THIS TIME)! try again...");
         playerDeck.push(playerCard);
